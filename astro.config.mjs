abb086c5cf1dc83e4a2ad8e9d6a49c5627e847fa
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-05-07 16:49:55 +0200

    fix base root in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://a160v.github.io/",
-  base: "/main/",
+  base: "/",
   integrations: [
     tailwind(),
     image({
